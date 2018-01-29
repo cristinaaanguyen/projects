0dbb242e23926e1906a8fb4a195d6b32b7a24c44
cristina nguyen <cristimn@uci.edu>
2018-01-28 21:32:41 -0800

    adding project2 files

---
+++ Fabflix/src/loginServlet.java
@@ -0,0 +1,106 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request,response);
+		
+	}
+	
+	
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		String loginUser = "mytestuser";
+        String loginPasswd = "mypassword";
+        String loginUrl = "jdbc:mysql:///moviedb?autoReconnect=true&useSSL=false";
+        response.setContentType("application/json"); // Response mime type
+        
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+        //System.out.println("Before try loop");
+        try {
+        		//System.out.println("Beginning of try loop");
+            //Class.forName("org.gjt.mm.mysql.Driver");
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println("Before Connection");
+            Connection dbcon = DriverManager.getConnection(loginUrl, loginUser, loginPasswd);
+            // Declare our statement
+            Statement statement = dbcon.createStatement();
+            
+            String query = "SELECT * from customers where customers.email = " + "'"+ email+ "'" + " AND customers.password = " + "'"+password+ "'";
+           // System.out.println(query);
+            // Perform the query
+            ResultSet rs = statement.executeQuery(query);
+            
+           // JsonArray jsonArray = new JsonArray();
+            JsonObject jsonObject = new JsonObject();
+            if (rs.next()) {
+            	
+	            String customer_email = rs.getString(6);
+	            String customer_password = rs.getString(7);
+	            jsonObject.addProperty("email", customer_email);
+	            jsonObject.addProperty("password", customer_password);
+            }
+            
+            out.write(jsonObject.toString());
+ 
+            rs.close();
+            statement.close();
+            dbcon.close();
+        } catch (SQLException ex) {
+            while (ex != null) {
+                System.out.println("SQL Exception:  " + ex.getMessage());
+                ex = ex.getNextException();
+            } // end while
+        } // end catch SQLException
+
+        catch (java.lang.Exception ex) {
+            out.println("<HTML>" + "<HEAD><TITLE>" + "MovieDB: Error" + "</TITLE></HEAD>\n<BODY>"
+                    + "<P>SQL error in doPost: " + ex.getMessage() + "</P></BODY></HTML>");
+            return;
+        }
+        out.close();
+		//doGet(request, response);
+	}
+
+}
